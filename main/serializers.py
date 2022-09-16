177d6f26e3a3b1374583b56c4dbca3f72b06bf7b
WiktorBoro <105320026+WiktorBoro@users.noreply.github.com>
2022-09-16 17:38:40 +0200

    Created serializers models, readability corrections necessary for ShortenedLinkSerializer function

--- main/serializers.py
+++ main/serializers.py
@@ -1,14 +1,37 @@
 from rest_framework import serializers
-from .models import Links
+from .models import Links, UsersInfo
 
 
-class LinkShortenerSerializer(serializers.ModelSerializer):
+class UserInfoSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Links
-        fields = '__all__'
+        model = UsersInfo
+        fields = ('user_agent', 'ip')
 
     def create(self, data):
-        print("serializacja danych", data)
-        shortened_link = Links.objects.get_or_create(**data)
+        users_info = UsersInfo.objects.get_or_create(ip=self.context.get('ip'),
+                                                     user_agent=self.context.get('user_agent'))
+
+        return users_info
+
+
+class ShortenedLinkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Links
+        fields = ['original_link', 'shortened_link']
+
+    def create(self, data):
+        user_info, user_exist = UsersInfo.objects.get_or_create(user_agent=self.context.get('user_agent'),
+                                                                ip=self.context.get('ip'))
+        shortened_link, link_exist = Links.objects.get_or_create(original_link=data['original_link'],
+                                                                 user_info=user_info)
+        # if link_exist:
+        #     return shortened_link, link_exist
 
         return shortened_link
+
+
+class LinkInputSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Links
+        fields = ['original_link']
