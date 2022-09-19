347e4983a4645209495c9d140a2735acae76cdc8
WiktorBoro <105320026+WiktorBoro@users.noreply.github.com>
2022-09-19 16:07:47 +0200

    Added heroku deploy staff

--- drf_link_shortener/settings.py
+++ drf_link_shortener/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q@qp%f$l*!gw(8x*j4xu^6t&m475_lhghwy!!*%yxq$ptztjb3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
