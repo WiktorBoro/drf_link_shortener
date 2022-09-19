f3ec11aa0ff4c4c145145bc0cc49b49a5149a2c3
WiktorBoro <105320026+WiktorBoro@users.noreply.github.com>
2022-09-19 15:48:21 +0200

    Fixed empty original_link error, added shortened_link as read_only, removed redundant serializer (LinkInput and UserInfo)

--- main/serializers.py
+++ main/serializers.py
@@ -2,36 +2,20 @@ from rest_framework import serializers
 from .models import Links, UsersInfo
 
 
-class UserInfoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UsersInfo
-        fields = ('user_agent', 'ip')
-
-    def create(self, data):
-        users_info = UsersInfo.objects.get_or_create(ip=self.context.get('ip'),
-                                                     user_agent=self.context.get('user_agent'))
-
-        return users_info
-
-
 class ShortenedLinkSerializer(serializers.ModelSerializer):
+    shortened_link = serializers.URLField(read_only=True)
+
     class Meta:
         model = Links
         fields = ['original_link', 'shortened_link']
+        extra_kwargs = {'original_link': {'required': True,
+                                          'allow_null': False}}
 
     def create(self, data):
         user_info, user_exist = UsersInfo.objects.get_or_create(user_agent=self.context.get('user_agent'),
                                                                 ip=self.context.get('ip'))
-        shortened_link, link_exist = Links.objects.get_or_create(original_link=data['original_link'],
-                                                                 user_info=user_info)
-        # if link_exist:
-        #     return shortened_link, link_exist
+
+        shortened_link = Links.objects.create(original_link=data['original_link'],
+                                              user_info=user_info)
 
         return shortened_link
-
-
-class LinkInputSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Links
-        fields = ['original_link']
